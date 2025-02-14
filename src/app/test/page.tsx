b4fb26282498b93558f3b4d99451ab7680368fe5
Jinsu <injnamek@gmail.com>
2025-02-14 12:44:13 +0900

    feat: 동적, 정적 앱 라우팅

---
+++ src/app/test/page.tsx
@@ -0,0 +1,13 @@
+// app > test > page.tsx
+import React from "react";
+
+const TestPage = () => {
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <p>Hello Next</p>
+    </div>
+  );
+};
+
+export default TestPage;
